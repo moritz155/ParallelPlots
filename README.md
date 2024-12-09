a5ed0e6eaa0dfd7de008ce69e6115ce5c288a4bd
Leon Haufe <leon.haufe@campus.tu-berlin.de>
2024-12-09 16:57:55 +0100

    removed the "Tests" from the src added parameter (bool) to normalize input data

--- README.md
+++ README.md
@@ -4,3 +4,42 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://moritz155.github.io/ParallelPlots.jl/dev/)
 [![Build Status](https://github.com/moritz155/ParallelPlots.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/moritz155/ParallelPlots.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/moritz155/ParallelPlots.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/moritz155/ParallelPlots.jl)
+
+## General
+This Project is for the TU-Berlin Course "Julia Programming for Machine Learning"
+Please make sure, that Julia `1.10` is used!
+
+_It was created with PkgTemplates.jl_
+
+## Getting Started
+
+### Install & Dependencies
+Please refer to this [Link](https://adrianhill.de/julia-ml-course/lectures/E1_Installation.html) for Installation of Julia
+
+You need to install differente packages.
+1. Open Julia with `julia` in your command prompt
+2. open the package manager with `]`
+<br> you should now see  
+3. Add the following modules:
+   1. `add CairoMakie`
+   2. `add DataFrames`
+   3. `add Random`
+
+### Usage
+you can create a plot with the following command
+`create_parallel_coordinates_plot(data::DataFrame)`
+
+you can call the function with the following parameter
+1. **data**<br>
+Dataframe: Where each Column will be a Dataset on the X-Axis
+2. **normalize**<br>
+Boolean: If Data should be normalized
+
+
+### Working on this Package
+- Adding Dependencies
+  - `activate /path/to/package` or `activate .` when in package
+  - `add DepName`
+- Tests
+  - f
+
--- src/ParallelPlots.jl
+++ src/ParallelPlots.jl
@@ -1,18 +1,11 @@
 module ParallelPlots
+
+    export create_parallel_coordinates_plot
+
 using CairoMakie
 using DataFrames
-using Random
 
-# Generate sample multivariate data
-Random.seed!(10)
-n_samples = 10
-df = DataFrame(
-    height=randn(n_samples),
-    weight=randn(n_samples),
-    age=rand(0:70, n_samples), # random numbers between 0 and 70
-    income=randn(n_samples),
-    education_years=rand(0:25, n_samples) # random numbers between 0 and 70
-)
+
 function normalize(data::DataFrame)
     normalized_data = copy(data)
     for col in names(data)
@@ -21,9 +14,13 @@ function normalize(data::DataFrame)
     end
 end
 # create parallel coordinates plot
-function create_parallel_coordinates_plot(data::DataFrame)
-    # normalized_data = normalize(data)
-    normalized_data = data
+function create_parallel_coordinates_plot(data::DataFrame; normalize=false)
+
+    # normalize when user parameter normalize == true
+    if normalize
+        data = normalize(data)
+    end
+
     # create figure and axis
     fig = Figure(figsize=(12, 6))
     ax = Axis(fig[1, 1],
@@ -36,10 +33,10 @@ function create_parallel_coordinates_plot(data::DataFrame)
     columns = names(data)
     n_cols = length(columns)
 
-    for row in 1:nrow(normalized_data)
+    for row in 1:nrow(data)
         line_points = Point2f[]
         for (i, col) in enumerate(columns)
-            push!(line_points, Point2f(i, normalized_data[row, col]))
+            push!(line_points, Point2f(i, data[row, col]))
         end
         lines!(ax, line_points, color=(:blue, 0.1))
     end
@@ -53,9 +50,6 @@ function create_parallel_coordinates_plot(data::DataFrame)
     return fig
 end
 
-#display
-fig = create_parallel_coordinates_plot(df)
-save("parallel_coordinates_plot.png", fig)
-display(fig)
+
 
 end
