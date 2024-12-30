c905d5164be9bb3a27284f9657aafb63c55765d0
smo <moritz.schelten@fokus.fraunhofer.de>
2024-12-30 12:53:03 +0100

    add test for false input

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,6 +21,29 @@ function create_person_df()
     return df
 end
 
+@testset "ArgumentError Tests" begin
+    df_missing = DataFrame(Name=["Alice", "Bob", "Charlie"],
+        Age=[25, 30, missing],
+        City=["Berlin", "Munich", "Hamburg"])
+    df_one_column = DataFrame(Name=["Alice", "Bob", "Charlie"])
+    df_one_line = DataFrame(Name=["Alice"],
+        Age=[25,],
+        City=["Berlin"])
+    # Test 1: Function throws ArgumentError for invalid input
+    @test_throws ArgumentError begin
+        ParallelPlots.create_parallel_coordinates_plot(DataFrame(nothing))
+    end
+
+    @test_throws ArgumentError begin
+        ParallelPlots.create_parallel_coordinates_plot(df_missing)
+    end
+    @test_throws ArgumentError begin
+        ParallelPlots.create_parallel_coordinates_plot(df_one_column)
+    end
+    @test_throws ArgumentError begin
+        ParallelPlots.create_parallel_coordinates_plot(df_one_line)
+    end
+end
 @testset "default call" begin
 
     # Generate sample multivariate data
